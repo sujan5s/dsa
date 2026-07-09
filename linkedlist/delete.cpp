b16c17629f5581d97e8df5d29186e97c656b6b86
Sujan S <sujanshivananda5@gmail.com>
2026-07-09 12:58:15 +0530

    Refactor code structure for improved readability and maintainability

---
+++ linkedlist/delete.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    public:
+        int data;
+        Node* next;
+
+    Node (int data1,Node* next1){
+        data=data1;
+        next=next1;
+    }
+    Node (int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* arrToLL(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+Node* Delete (Node* head){
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+    return  head;
+}
+
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* y = new Node(arr[0],nullptr);
+    Node* head = arrToLL(arr);
+    
+    Delete(head);
+    Node* temp = head;
+    while(temp){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
