50c5d831e27de92b788f771527cc41d090dd6f9e
Sujan S <sujanshivananda5@gmail.com>
2026-07-11 09:44:03 +0530

    feat: implement insertK function to insert a node at the k-th position in linked list

--- linkedlist/insert.cpp
+++ linkedlist/insert.cpp
@@ -15,3 +15,26 @@ Node* insertTail ( Node* head,int val){
     temp->next = newNode;
     return head;
 }
+
+Node* insertK ( Node* head,int val,int k){
+    if(head==NULL){
+        return new Node(val);
+    }
+    if(k==1){
+        Node* temp = new Node(val,head);
+        return temp;
+    }
+    int cnt=0;
+    Node* temp=head;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt==k-1){
+            Node* x = new Node(val);
+            x->next = temp->next;
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
