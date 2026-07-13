833c18fbf9967a6acbdb8cf66d3b8375a8f25711
Sujan S <sujanshivananda5@gmail.com>
2026-07-14 00:09:25 +0530

    Refactor code structure for improved readability and maintainability

---
+++ linkedlist/DLL/dhead.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* back;
+
+    Node (int data1,Node* next1,Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node (int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+
+Node* convertArrToDll(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next= temp;
+        prev = temp;
+    }
+    return head;
+}
+
+Node* DHead(Node* head){
+    if(head==NULL ||head->next==NULL ){
+        return NULL;
+    }
+    Node* temp = head;
+    head = head->next;
+    head->back=nullptr;
+    temp->next=nullptr;
+    delete temp;
+    return head;
+}
+
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* head = convertArrToDll(arr);
+    head=DHead(head);
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
