cb7855641ef14cc10c69c9672b7908b221b9ed58
Sujan S <sujanshivananda5@gmail.com>
2026-07-15 16:23:13 +0530

    Refactor code structure for improved readability and maintainability

---
+++ linkedlist/DLL/dtail.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+        Node* back;
+
+    Node (int data1,Node* next1,Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node (int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+};
+
+Node* convertArrToDll(vector<int> arr){
+    Node* head = new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i],nullptr,prev);
+        prev->next= temp;
+        prev = temp;
+    }
+    return head;
+}
+
+Node* DTail(Node* head){
+    if(head==NULL || head->next==NULL){
+        return NULL;
+    }
+    Node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->back->next=nullptr;
+    temp->back=nullptr;
+    delete temp;
+    return head;
+}
+
+int main(){
+    vector<int> arr = {2,5,8,7};
+    Node* head = convertArrToDll(arr);
+    head=DTail(head);
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+}
